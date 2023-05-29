4d9fbbf085582baa421a664a90a49d0ab764e288
bulutaysarac <bulutaysarac@gmail.com>
2023-05-29 21:48:20 +0300

    RefAndOut

---
+++ Studies.RefAndOutKeywords/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Studies.RefAndOutKeywords
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+            Add(ref a, b);
+            //a = 8
+            int c;
+            Multiply(a, b, out c);
+            Console.WriteLine(a); //Output: 8
+            //c = 24
+            Console.WriteLine(c); //Output: 24
+            Add(ref c, b);
+            //c = 27
+            Console.WriteLine(c); //Output: 27
+        }
+
+        static void Add(ref int x, int y)
+        {
+            x = x + y;
+        }
+
+        static void Multiply(int x, int y, out int r)
+        {
+            r = x * y;
+        }
+    }
+}
