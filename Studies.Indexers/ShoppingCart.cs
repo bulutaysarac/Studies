a33b73752770a6cbf260ce2569faa7b66229c7ca
bulutaysarac <bulutaysarac@gmail.com>
2023-05-30 15:32:14 +0300

    OperatorOverloadingAndIndexers

---
+++ Studies.Indexers/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿namespace Studies.Indexers
+{
+    public class ShoppingCart
+    {
+        private List<string> items = new List<string>();
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < items.Count)
+                    return items[index];
+
+                throw new IndexOutOfRangeException("Invalid index");
+            }
+            set
+            {
+                if (index >= 0 && index < items.Count)
+                    items[index] = value;
+                else if (index == items.Count)
+                    items.Add(value);
+                else
+                    throw new IndexOutOfRangeException("Invalid index");
+            }
+        }
+    }
+}
---
+++ Studies.OperatorOverloading/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Studies.OperatorOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber c1 = new ComplexNumber(1, 2);
+            ComplexNumber c2 = new ComplexNumber(4, 7);
+            ComplexNumber c3;
+
+            c3 = c1 + c2;
+            Console.WriteLine($"c1 = {c1.Integer} + {c1.Imaginary}i");
+            Console.WriteLine($"c2 = {c2.Integer} + {c2.Imaginary}i");
+            Console.WriteLine($"c1 + c2 = {c3.Integer} + {c3.Imaginary}i");
+            c3 = c1 - c2;
+            Console.WriteLine($"c1 - c2 = {c3.Integer} + {c3.Imaginary}i");
+        }
+    }
+}
